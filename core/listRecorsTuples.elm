3897e373aacd62ab09c666f99328aff9baa3fbeb
Masiu <ciszekmarcell@gmail.com>
2019-09-28 08:40:59 +0200

    list tupples recors

---
+++ core/listRecorsTuples.elm
@@ -0,0 +1,22 @@
+names = ["aleks","boris","karol"]
+nums = [1,2,3,4,5,6,7,0]
+
+List.isEmpty names
+-- False
+List.reverse names
+List.sort names
+List.sort nums
+
+increment n =
+  n + 1
+
+List.map increment nums
+
+
+mike = {first = "mike" , last = "Smith", age = 21 }
+
+mike.first
+mike.last
+mike.age
+
+.last
---
+++ sandbox.elm
@@ -0,0 +1,24 @@
+import Html exposing (text)
+
+main =
+  devide 5 135
+    |> text
+
+devide: Float -> Float -> String
+devide a b =
+  a / b
+    |> toString
+
+-- Curring
+devideby5: Float -> String
+devideby5 =
+  devide 5
+
+
+isFactor a b =
+  if rem a b == 0 then
+    True
+  else
+    False
+
+
