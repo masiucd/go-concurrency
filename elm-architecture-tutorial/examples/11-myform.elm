507f8064cc4c2d617b3df03eb5292bddef42b831
Masiu <ciszekmarcell@gmail.com>
2019-09-29 13:41:56 +0200

    working on the input fields

---
+++ elm-architecture-tutorial/examples/11-myform.elm
@@ -0,0 +1,76 @@
+module Main exposing (..)
+
+import Browser
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
+
+
+main =
+    Browser.sandbox { init = init, update = update, view = view }
+
+
+type alias Model =
+    { name : String
+    , password : String
+    , passwordAgain : String
+    , age : Int
+    }
+
+
+init : Model
+init =
+    Model "" "" "" 0
+
+
+type Msg
+    = Name String
+    | Password String
+    | PasswordAgain String
+    | Age Int
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Name name ->
+            { model | name = name }
+
+        Password password ->
+            { model | password = password }
+
+        PasswordAgain password ->
+            { model | passwordAgain = password }
+
+        Age age ->
+            { model | age = age }
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ viewInput "text" "Name" model.name Name
+        , viewInput "password" "Password" model.password Password
+        , viewInput "password" "Re-enter Password" model.passwordAgain PasswordAgain
+        , viewInput2 "text" "enter your age" (String.fromInt model.age) Age
+        , viewValidation model
+        ]
+
+
+viewInput : String -> String -> String -> (String -> msg) -> Html msg
+viewInput t p v toMsg =
+    input [ type_ t, placeholder p, value v, onInput toMsg ] []
+
+
+viewInput2 : String -> String -> String -> (Int -> msg) -> Html msg
+viewInput2 t p v toAge =
+    input [ type_ t, placeholder p, value v, onInput toAge ] []
+
+
+viewValidation : Model -> Html msg
+viewValidation model =
+    if model.password == model.passwordAgain then
+        div [ style "color" "green" ] [ text "OK" ]
+
+    else
+        div [ style "color" "red" ] [ text "Passwords do not match!" ]
