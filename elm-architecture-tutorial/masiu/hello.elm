78e8a5ec577ac8619e62eb4f50106343b3b4292f
Masiu <ciszekmarcell@gmail.com>
2019-10-21 18:15:46 +0200

    elm funcs

--- elm-architecture-tutorial/masiu/hello.elm
+++ elm-architecture-tutorial/masiu/hello.elm
@@ -7,10 +7,25 @@ add a b =
     a + b
 
 
+checkPower level =
+    if checkPower > 100 then
+        "Yeahh booi"
+
+    else
+        "Meehh"
+
+
+
+-- result =
+--     add 10 12
+
+
 result =
-    add 10 12
+    -- add 10 12 |> add 10
+    add 2 2 |> (\a -> a remainderBy 2 == 0)
 
 
 main =
     -- Html.text (String.concat [ "Hello Elm" ])
+    -- Html.text (String.fromInt result)
     Html.text (String.fromInt result)
