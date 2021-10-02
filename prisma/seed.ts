f9bf5226465f25fd0ed284f7b36de7671295ca94
masiucd <ciszekmarcell@gmail.com>
2021-10-02 16:21:58 +0200

    seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,18 +3,55 @@ import prisma from "../lib/prisma"
 
 export async function main() {
   try {
-    const result = await prisma.$queryRaw`
+    await prisma.$queryRaw`
     INSERT INTO movie ( title, releaseYear, price, rating,image,slug)
       values (
         'Die hard',
         1998,
         20500,
         4,
-        'die-hard-svg',
+        'die-hard.svg',
         'die-hard'
+      ),
+       (
+        'Inception',
+        2010,
+        50500,
+        5,
+        'inception.svg',
+        'inception'
+      ),
+       (
+        'Interstellar',
+        2014,
+        62500,
+        5,
+        'interstellar.svg',
+        'interstellar'
+      ),
+       (
+        'Fight Club',
+        1999,
+        32500,
+        3,
+        'fight-club.svg',
+        'fight-club'
+      ),
+       (
+        'No Time to Die',
+        2021,
+        89500,
+        5,
+        'no-time-to-die.svg',
+        'no-time-to-die'
+      );
+
+      INSERT INTO category (name, movies)
+      values(
+        'horror'
+          
       );
     `
-    console.log(result)
   } catch (err) {
     console.error(err)
     process.exit(1)
