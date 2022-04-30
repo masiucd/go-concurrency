ee6513678871708e6bd3309dec88d61b56db2d89
masiucd <ciszekmarcell@gmail.com>
2022-04-30 10:06:56 +0200

    Without waitgroups

--- src/wait-groups/with-wait-group.go
+++ src/wait-groups/with-wait-group.go
@@ -12,7 +12,7 @@ func run() {
 	start := time.Now()
 	go goForARun(&wg)
 	go swiming(&wg)
-	wg.Wait() // block the code
+	wg.Wait() // block the code until finishes
 
 	fmt.Println("Guess we are done!!")
 	elapsed := time.Since(start)
