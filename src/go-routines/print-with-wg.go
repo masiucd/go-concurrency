289d06d8f69405e21aa74627f4d435cb15d0a83c
masiucd <ciszekmarcell@gmail.com>
2022-05-26 13:07:40 +0200

    go routines with wait groups

--- src/go-routines/print-with-wg.go
+++ src/go-routines/print-with-wg.go
@@ -1,28 +1,29 @@
-package main
+package goroutines
 
 import (
 	"fmt"
 	"sync"
 )
 
-func run(wg *sync.WaitGroup) {
+func run2(wg *sync.WaitGroup) {
 	defer wg.Done()
 	for i := 0; i < 5; i++ {
 		fmt.Println("i is ", i)
 	}
 }
 
-func greeting(wg *sync.WaitGroup) {
+func greeting2(wg *sync.WaitGroup) {
 	defer wg.Done()
 	fmt.Println("Hello")
 }
 
-func main() {
+// InitWithWait init
+func InitWithWait() {
 
 	var wg sync.WaitGroup
 	wg.Add(2)
-	go run(&wg)      // pushed to the stack
-	go greeting(&wg) // pushed to the stack
+	go run2(&wg)      // pushed to the stack
+	go greeting2(&wg) // pushed to the stack
 	// greeting get popped first
 	// run get's popped last
 	wg.Wait()
